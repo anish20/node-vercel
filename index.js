51fa3b282248150f58d2c1f2eed8e8606ee3fe43
Anish <anish15.786@gmail.com>
2024-06-17 21:52:10 +0530

    node setup

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-// Import packages
-const express = require("express");
-const home = require("./routes/home");
+const express=require("express");
+require('dotenv').config()
+const cors=require("cors");
+const { router } = require("./routes");
+const { indexTaskSchedulder } = require("./scheduler");
+const fileUpload = require("express-fileupload");
+const { BASE_URL } = require("./config/environmentVariable");
+const mongoose = require('mongoose');
 
-// Middlewares
-const app = express();
-app.use(express.json());
+const app=express();
+const PORT=process.env.PORT || 9001;
+// let BASE_URL= process.env.NODE_ENV==='development' ? process.env.APP_URL_DEV : process.env.APP_URL
 
-// Routes
-app.use("/home", home);
+// Middleware configure
+app.use(cors())
+app.use(express.json())
+app.use(fileUpload())
+app.use(express.static("public")); // middleware for access to upload inside public folder
+app.use(express.urlencoded({extended:false}))
+app.use("/insurance_crm/api",router)
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+
+//Global Error handler
+
+// Job scheduler
+indexTaskSchedulder();
+
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    return res.status(statusCode).json(err?.message)
+})
+
+app.use("/",(req,res)=>{
+    res.send("Welcome to my API")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server Runing on PORT  ${PORT}`);
+    console.log(process.env.NODE_ENV);
+    console.log(BASE_URL);
+
+
+})
